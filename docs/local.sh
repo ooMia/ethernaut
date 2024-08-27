e2235e6305adfb440a4e283751eda5d710e0a4a0
Hyeon-hak <hyeonhak.kim.dev@gmail.com>
2024-08-28 02:25:18 +0900

    📝 Docs : scripts

---
+++ docs/local.sh
@@ -0,0 +1,19 @@
+# local
+
+# ----- 0 -----
+export RPC=http://localhost:8545
+export PK=ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+export ADR=0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+
+
+export LV=0xd43F5E24C6b2edc1dFDE8548cDdaBeFFA6eCc822
+
+
+
+This instance represents a Good Samaritan that is wealthy and ready to donate some coins to anyone requesting it.
+
+Would you be able to drain all the balance from his Wallet?
+
+Things that might help:
+
+Solidity Custom Errors
