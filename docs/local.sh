4552c9b50d251f716963df5a303316b8e1ca70f5
Hyeon-hak <hyeonhak.kim.dev@gmail.com>
2024-08-28 11:01:51 +0900

    ✨ Feat : solve all required

--- docs/local.sh
+++ docs/local.sh
@@ -6,14 +6,4 @@ export PK=ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 export ADR=0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
 
 
-export LV=0xd43F5E24C6b2edc1dFDE8548cDdaBeFFA6eCc822
-
-
-
-This instance represents a Good Samaritan that is wealthy and ready to donate some coins to anyone requesting it.
-
-Would you be able to drain all the balance from his Wallet?
-
-Things that might help:
-
-Solidity Custom Errors
+export LV=0x8C3eb19Be421F57E94dE0AFB6045a7b6418a7cd7
